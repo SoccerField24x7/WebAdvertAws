2e0cfbf41c95867d8715cf4e219277c3626fbeed
Jesse Quijano <jesse@quijano.net>
2021-06-16 13:38:11 -0700

    move application/json config to each method - which actually makes more sense

--- WebAdvert.Web/ServiceClients/AdvertApiClient.cs
+++ WebAdvert.Web/ServiceClients/AdvertApiClient.cs
@@ -7,6 +7,7 @@ using Newtonsoft.Json;
 using AutoMapper;
 using System.Net;
 using System.Net.Http.Headers;
+using System.Text;
 
 namespace WebAdvert.Web.ServiceClients
 {
@@ -24,7 +25,7 @@ namespace WebAdvert.Web.ServiceClients
 
             var createUrl = _config.GetSection("AdvertApi").GetValue<string>("BaseUrl");
             _client.BaseAddress = new Uri(createUrl);
-            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //_client.DefaultRequestHeaders.Add("Content-Type", "application/json");
         }
 
         public async Task<bool> ConfirmAsync(ConfirmAdvertRequest model)
@@ -32,7 +33,7 @@ namespace WebAdvert.Web.ServiceClients
             var advertModel = _mapper.Map<ConfirmAdvertModel>(model);
             var jsonModel = JsonConvert.SerializeObject(advertModel);
 
-            var response = await _client.PutAsync(new Uri($"{_client.BaseAddress}/confirm"), new StringContent(jsonModel));
+            var response = await _client.PutAsync(new Uri($"{_client.BaseAddress}/confirm"), new StringContent(jsonModel, Encoding.UTF8, "application/json"));
 
             return response.StatusCode == HttpStatusCode.OK;
         }
@@ -41,7 +42,7 @@ namespace WebAdvert.Web.ServiceClients
         {
             var advertApiModel = _mapper.Map<AdvertModel>(model);
             var jsonModel = JsonConvert.SerializeObject(model);
-            var response = await _client.PostAsync(new Uri($"{_client.BaseAddress}/create"), new StringContent(jsonModel));
+            var response = await _client.PostAsync(new Uri($"{_client.BaseAddress}/create"), new StringContent(jsonModel, Encoding.UTF8, "application/json"));
 
             var responseJson = await response.Content.ReadAsStringAsync();
 
