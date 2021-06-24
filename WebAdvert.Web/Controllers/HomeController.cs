19fabbd75760cc39c494ec0b3046a1f6912effd8
Jesse Quijano <jesse@quijano.net>
2021-06-23 17:12:01 -0700

    search and index methods

--- WebAdvert.Web/Controllers/HomeController.cs
+++ WebAdvert.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -7,23 +8,50 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using WebAdvert.Web.Models;
+using WebAdvert.Web.Models.Search;
+using WebAdvert.Web.ServiceClients;
 
 namespace WebAdvert.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ISearchApiClient _searchApiClient;
+        private readonly IMapper _mapper;
+        private readonly IAdvertApiClient _apiClient;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ISearchApiClient searchApiClient, IMapper mapper, IAdvertApiClient apiClient)
         {
             _logger = logger;
+            _searchApiClient = searchApiClient;
+            _mapper = mapper;
+            _apiClient = apiClient;
         }
 
         [Authorize]
-        public IActionResult Index()
+        [ResponseCache(Duration = 60)]
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var allAds = await _apiClient.GetAllAsync();
+            var allViewModels = allAds.Select(x => _mapper.Map<IndexViewModel>(x));
+
+            return View(allViewModels);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Search(string keyword)
+        {
+            var viewModel = new List<SearchViewModel>();
+            var searchResult = await _searchApiClient.Search(keyword);
+            searchResult.ForEach(advertDoc =>
+            {
+                var viewModelItem = _mapper.Map<SearchViewModel>(advertDoc);
+                viewModel.Add(viewModelItem);
+            });
+
+            return View("Search", viewModel);
+        }
+
 
         public IActionResult Privacy()
         {
